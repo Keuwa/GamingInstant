24f12483a35ba1a8d96fcbc87b2cf8a14cad8f07
Keuwa <vincent.durpoix@gmail.com>
2017-10-08 16:33:35 +0200

    getList ok no filter

--- actions/game/find.js
+++ actions/game/find.js
@@ -13,13 +13,12 @@ module.exports = function(server) {
     // TODO: Add filter of exclusion, age/price, played game ....
     models.Game.findAll({
     }).then(function (instanceGame) {
-      console.log("aaaaaaaaaaaaaaaaaa");
       if(!instanceGame) return res.status(404).send("Not found");
         //filterResult(instanceGame)
         let promises = [];
         for(let i in instanceGame){
           let options = {
-            url: 'http://store.steampowered.com/api/appdetails?appids='+instanceGame.appid,
+            url: 'http://store.steampowered.com/api/appdetails?appids='+instanceGame[i].appid,
             json: true,
             headers: {
               'content-type': 'application/json; charset=utf-8'
@@ -28,9 +27,32 @@ module.exports = function(server) {
           promises.push(request.get(options))
         }
       Promise.all(promises).then(function (gameList) {
-        let results = {}
-        return res.status(200).send(instanceGame);
+        let results = []
+        for(let i in gameList){
+            let gameId = Object.keys(gameList[i])[0]
+            console.log("id : ",gameId," //// ",gameList[i][gameId]);
+            let price;
+            if(gameList[i][gameId].success != false){
+              if(typeof(gameList[i][gameId].data.price_overview)=="undefined"){
+                price = 0
+              }else{
+                price = gameList[i][gameId].data.price_overview.initial/100
+              }
+              results.push({
+                name:gameList[i][gameId].data.name,
+                required_age:gameList[i][gameId].data.required_age,
+                detailed_description:gameList[i][gameId].data.detailed_description,
+                price:price,
+                category:gameList[i][gameId].data.genres,
+                imageUrl:typeof(gameList[i][gameId].data.screenshots)=="undefined"?"http://www.jblog.fr/images/mini_500_noimg.gif":gameList[i][gameId].data.screenshots[0].path_thumbnail
+              })
+          }
+        }
+        return res.status(200).send(results);
 
+      }).catch(function (err) {
+        console.error(err);
+        return res.status(500).send("ERROR");
       })
 
     }).catch(function (err) {
